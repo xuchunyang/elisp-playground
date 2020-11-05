6745c6431944c3c5c87097aeecdc14dae8bada35
Chunyang Xu <mail@xuchunyang.me>
2020-11-05 21:31:59 +0800

    Accept code by POST JSON

--- server.js
+++ server.js
@@ -19,27 +19,91 @@ const evalEmacsLispCode = async (code) => {
         return;
       }
       const idx = stderr.lastIndexOf(prefix);
-      const value = stderr.slice(idx + prefix.length);
+      const value = stderr.slice(
+        idx + prefix.length,
+        // remove trailing \n added by message
+        stderr.length - 1
+      );
       stderr = stderr.slice(0, idx);
       resole({ stdout, stderr, value });
     });
   });
 };
 
+const getReqBody = (req) => {
+  return new Promise((resolve, reject) => {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      resolve(body);
+    });
+    req.on("error", (err) => {
+      reject(err);
+    });
+  });
+};
+
 http
   .createServer(async (req, res) => {
-    res.setHeader("Content-Type", "application/json; charset=UTF-8");
-    let data = null;
+    res.status = (statusCode) => {
+      res.statusCode = statusCode;
+      return res;
+    };
+    res.json = (data) => {
+      res.setHeader("Content-Type", "application/json; charset=UTF-8");
+      res.end(JSON.stringify(data, null, 2) + "\n");
+      return res;
+    };
+    res.error = (message) => {
+      res.status(400).json({ error: message });
+    };
+
+    if (req.method !== "POST") {
+      res.error({ error: "HTTP method is not POST" });
+      return;
+    }
+    const contentType = req.headers["content-type"];
+    if (!contentType) {
+      res.error("Content-Type is missing");
+      return;
+    }
+    if (!/^application\/json/i.test(contentType)) {
+      res.error("Content-Type is not JSON");
+      return;
+    }
+
+    const reqBody = await getReqBody(req);
+    if (!reqBody) {
+      res.error(`Empty body, you need POST {"code": "(+ 1 2)"}`);
+      return;
+    }
+    console.log(reqBody);
+
+    let resJson;
     try {
-      data = await evalEmacsLispCode(CODE);
+      resJson = JSON.parse(reqBody);
+    } catch (err) {
+      res.error(`JSON.parse error! ${err.message}`);
+      return;
+    }
+    const code = resJson.code;
+    if (!code) {
+      res.error(`Missing code, you need POST {"code": "(+ 1 2)"}`);
+      return;
+    }
+
+    try {
+      const result = await evalEmacsLispCode(code);
+      res.status(200).json(result);
     } catch (err) {
       // console.error("ERROR\n", err);
-      data = {};
-      Object.assign(data, err);
-      data.error_message = err.message;
-      res.statusCode = 400;
+      const myError = {};
+      Object.assign(myError, err);
+      myError.error_message = err.message;
+      res.status(400).json(myError);
     }
-    res.end(JSON.stringify(data, null, 2) + "\n");
   })
   .listen(3000, () => {
     console.log("Listening at http://127.0.0.1:3000/");
