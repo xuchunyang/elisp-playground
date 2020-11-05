13a9b297af250b22f035e9687d09d2c780985d32
Chunyang Xu <mail@xuchunyang.me>
2020-11-06 01:16:50 +0800

    Add Access-Control-Max-Age to let browser to cache OPTIONS

--- server.js
+++ server.js
@@ -62,12 +62,13 @@ http
 
     res.setHeader("Access-Control-Allow-Origin", "*");
     if (req.method === "OPTIONS") {
-      res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+      res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
       res.setHeader(
         "Access-Control-Allow-Headers",
         "X-PINGOTHER, Content-Type"
       );
-      res.status(200).end();
+      res.setHeader("Access-Control-Max-Age", "86400");
+      res.status(204).end();
       return;
     }
 
