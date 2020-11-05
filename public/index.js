71761c6202033154a10f919f989dc096ad9dcf39
Chunyang Xu <mail@xuchunyang.me>
2020-11-06 02:01:37 +0800

    * public/index.js: Update.

--- public/index.js
+++ public/index.js
@@ -22,5 +22,18 @@ form.onsubmit = async (e) => {
   console.log(response);
   const json = await response.json();
   console.log(json);
-  output.innerHTML = `<pre>${JSON.stringify(json, null, 2)}</pre>`;
+  if ("error" in json) {
+    output.innerHTML = `<pre>${JSON.stringify(json, null, 2)}</pre>`;
+    return;
+  }
+  output.innerHTML = `
+<p>Value:</p>
+<pre>${json.value}</pre>
+
+<p>Stdout:</p>
+<pre>${json.stdout}</pre>
+
+<p>Stderr:</p>
+<pre>${json.stderr}</pre>
+`;
 };
