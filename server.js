84e8865e5e7a776a135f46d36e7b4f3726d7554c
Chunyang Xu <mail@xuchunyang.me>
2020-11-09 15:14:31 +0800

    Switch to express and support multiple versions of Emacs [#2]

--- server.js
+++ server.js
@@ -1,22 +1,60 @@
 require("dotenv").config();
-const http = require("http");
 const { execFile } = require("child_process");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/", async (req, res) => {
+  const { version, code } = req.body;
+  if (!code) {
+    res.status(400).json({ error: "Missing code" });
+    return;
+  }
+  if (version && !check_emacs_batch_command(version)) {
+    res
+      .status(400)
+      .json({ error: `Invalid version, supports ${VERSIONS.join(", ")}` });
+    return;
+  }
+
+  try {
+    const result = await evalEmacsLispCode(code, version);
+    res.json(result);
+  } catch (err) {
+    // console.error("ERROR\n", err);
+    const myError = {};
+    Object.assign(myError, err);
+    myError.error = err.message;
+    res.status(400).json(myError);
+  }
+});
 
 // From fast to slow
 //
 // emacs -Q --batch --eval '(pp emacs-version)'
 // unshare --fork --pid --mount-proc -w /tmp -S 1001 -G 1001 emacs -Q --batch --eval '(pp emacs-version)'
 // podman run --rm --pids-limit 2 --cpus=".5" -m 100m --read-only --network none silex/emacs emacs -Q --batch --eval '(pp emacs-version)'
-const EMACS_BATCH_COMMAND = (
-  process.env.EMACS_BATCH_COMMAND ||
-  "podman run --rm --read-only --network none silex/emacs emacs -Q --batch --eval"
-).split(" ");
+const EMACS_BATCH_COMMAND = process.env.EMACS_BATCH_COMMAND;
 
-const evalEmacsLispCode = async (code) => {
+const VERSIONS = ["24.5", "25.3", "26.3", "27.1"];
+const check_emacs_batch_command = (emacsVersion) => {
+  if (EMACS_BATCH_COMMAND) return true;
+  return versions.includes(emacsVersion);
+};
+const build_emacs_batch_command = (emacsVersion) => {
+  const commandString = EMACS_BATCH_COMMAND
+    ? EMACS_BATCH_COMMAND
+    : `podman run --rm --read-only --network none silex/emacs:${emacsVersion} emacs -Q --batch --eval`;
+  return commandString.split(" ");
+};
+
+const evalEmacsLispCode = async (code, version) => {
   const start_time = new Date();
   const prefix = "\n#+RESULTS:\n";
   const wrapper = `(message "${prefix}%s" (pp-to-string (progn ${code})))`;
-  const command = EMACS_BATCH_COMMAND.concat(wrapper);
+  const command = build_emacs_batch_command(version).concat(wrapper);
   const options = {
     timeout: 1000 * 10, // 10 seconds
     maxBuffer: 1024 * 100, // 100 KiB
@@ -52,94 +90,8 @@ evalEmacsLispCode("(+ 1 2)")
     // console.error(e);
   });
 
-const getReqBody = (req) => {
-  return new Promise((resolve, reject) => {
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-    req.on("end", () => {
-      resolve(body);
-    });
-    req.on("error", (err) => {
-      reject(err);
-    });
-  });
-};
-
-http
-  .createServer(async (req, res) => {
-    res.status = (statusCode) => {
-      res.statusCode = statusCode;
-      return res;
-    };
-    res.json = (data) => {
-      res.setHeader("Content-Type", "application/json; charset=UTF-8");
-      res.end(JSON.stringify(data, null, 2) + "\n");
-      return res;
-    };
-    res.error = (message) => {
-      res.status(400).json({ error: message });
-    };
-
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    if (req.method === "OPTIONS") {
-      res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-      res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-PINGOTHER, Content-Type"
-      );
-      res.setHeader("Access-Control-Max-Age", "86400");
-      res.status(204).end();
-      return;
-    }
-
-    if (req.method !== "POST") {
-      res.error("HTTP method is not POST");
-      return;
-    }
-    const contentType = req.headers["content-type"];
-    if (!contentType) {
-      res.error("Content-Type is missing");
-      return;
-    }
-    if (!/^application\/json/i.test(contentType)) {
-      res.error("Content-Type is not JSON");
-      return;
-    }
-
-    const reqBody = await getReqBody(req);
-    if (!reqBody) {
-      res.error(`Empty body, you need POST {"code": "(+ 1 2)"}`);
-      return;
-    }
-    console.log(reqBody);
-
-    let resJson;
-    try {
-      resJson = JSON.parse(reqBody);
-    } catch (err) {
-      res.error(`JSON.parse error! ${err.message}`);
-      return;
-    }
-    const code = resJson.code;
-    if (!code) {
-      res.error(`Missing code, you need POST {"code": "(+ 1 2)"}`);
-      return;
-    }
-
-    console.log(code);
-    try {
-      const result = await evalEmacsLispCode(code);
-      res.status(200).json(result);
-    } catch (err) {
-      // console.error("ERROR\n", err);
-      const myError = {};
-      Object.assign(myError, err);
-      myError.error = err.message;
-      res.status(400).json(myError);
-    }
-  })
-  .listen(3000, () => {
-    console.log("Listening at http://0.0.0.0:3000/");
-  });
+const host = process.env.HOST || "localhost";
+const port = process.env.PORT || 3000;
+app.listen(port, host, () => {
+  console.log(`Listening at http://${host}:${port}`);
+});
